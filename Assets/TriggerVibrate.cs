420cd188555061eef27c956b8c9778c7967d0485
Kevin Watters <kevinwatters@gmail.com>
2016-04-02 19:43:47 -0700

    initial commit

---
+++ Assets/TriggerVibrate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerVibrate : MonoBehaviour {
+
+	public MonoBehaviour target;
+
+	void Update () {
+		target.enabled = Input.GetKey (KeyCode.Space);
+	}
+}
---
+++ Assets/UpdateTransform.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateTransform : MonoBehaviour {
+
+	public Vector3 rotationDelta;
+
+	void Update () {
+		transform.Rotate (rotationDelta * Time.deltaTime);
+	}
+}
